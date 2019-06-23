328d27f91b43a4079b6dd7872e2283644308d355
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-06-23 18:12:52 +0200

    Auth : Correcting Auth class instance in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:goodtime/services/Authentication.dart';
-import 'package:goodtime/routes/SignupAndLoginRoute.dart';
+import 'package:goodtime/services/FirebaseAuthentication.dart';
+import 'package:goodtime/routes/AppRootRoute.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Goodtime Login',
+      title: 'Goodtime',
       /*theme: ThemeData(
         primarySwatch: Colors.amber,
       ),*/
-      home: new SignupAndLoginRoute(auth: new Authentication())
+      home: new AppRootRoute(auth: new FirebaseAuthentication())
     );
   }
 }
