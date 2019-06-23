726425a2a511994bba92281a8077fa052e2bee34
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-06-23 14:32:23 +0200

    Auth : Defining Auth interface

---
+++ lib/services/BaseAuth.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+
+abstract class BaseAuth {
+  Future<String> signIn(String email, String password);
+  Future<String> signUp(String email, String password);
+  Future<String> getCurrentUser();
+  Future<void> signOut();
+}
