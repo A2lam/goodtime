ba55ae30a8c0ff119d4d7d6b51727d87b224fc18
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-07-10 00:18:34 +0200

    Bar : improving bars page design

--- lib/routes/BarRoute.dart
+++ lib/routes/BarRoute.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:goodtime/models/Bar.dart';
 import 'package:goodtime/services/BarService.dart';
+import 'package:goodtime/routes/BarDetailsRoute.dart';
 
 class BarRoute extends StatefulWidget {
   BarRoute({ Key key }) : super(key: key);
@@ -21,6 +22,7 @@ class _BarRouteState extends State<BarRoute> {
     super.initState();
   }
 
+  // TODO : RÉGLER LE SOUCIS DE LA DUPLICATION DE LISTE
   void filterSearchResults(String query) {
     List<Bar> duplicatedBarList = [];
     duplicatedBarList.addAll(_barList);
@@ -67,7 +69,77 @@ class _BarRouteState extends State<BarRoute> {
     );
   }
 
+  Widget makeListTile(Bar bar) => ListTile(
+    contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+    leading: Container(
+      padding: EdgeInsets.only(right: 12.0),
+      decoration: new BoxDecoration(
+        border: new Border(
+          right: new BorderSide(width: 1.0, color: Colors.white24)
+        )
+      ),
+      child: Icon(Icons.autorenew, color: Colors.black),
+    ),
+    title: Text(
+      bar.name,
+      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    ),
+    // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+    // TODO : Quand faudra rajouter un sous-titre
+    /*subtitle: Row(
+      children: <Widget>[
+        Expanded(
+            flex: 1,
+            child: Container(
+              // tag: 'hero',
+              child: LinearProgressIndicator(
+                  backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
+                  value: lesson.indicatorValue,
+                  valueColor: AlwaysStoppedAnimation(Colors.green)),
+            )),
+        Expanded(
+          flex: 4,
+          child: Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(lesson.level,
+                  style: TextStyle(color: Colors.white))),
+        )
+      ],
+    ),*/
+
+    trailing: Icon(Icons.keyboard_arrow_right, color: Colors.black, size: 30.0),
+    onTap: () {
+      Navigator.push(
+          this.context,
+          MaterialPageRoute(builder: (context) => BarDetailsRoute(bar: bar))
+      );
+    },
+  );
+
+  Widget makeCard(Bar bar) => Card(
+    elevation: 8.0,
+    margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+    child: Container(
+      decoration: BoxDecoration(color: Colors.grey[200]), // Color.fromRGBO(64, 75, 96, .9)
+      child: makeListTile(bar),
+    ),
+  );
+
   Widget _showBarList() {
+    return Expanded(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: _barList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard(_barList[index]);
+        }
+      )
+    );
+  }
+
+  /*Widget _showBarList() {
     return new Expanded(
         child: ListView.builder(
           itemCount: _barList.length,
@@ -78,7 +150,7 @@ class _BarRouteState extends State<BarRoute> {
           },
         )
     );
-  }
+  }*/
 
   Widget _showBody() {
     return new Column(
