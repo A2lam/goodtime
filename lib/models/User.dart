9584a78ea3c5793803b5383bfa9f853fc3cc3390
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-09-05 16:25:54 +0200

    Bar : (User) Creating User model

---
+++ lib/models/User.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class User {
+  int id;
+  String firstname;
+  String lastname;
+  String phone;
+  String username;
+  String email;
+  String password;
+  String token;
+  int favorite_item;
+  double max_price;
+  int favorite_transportation;
+  int created_by;
+  DateTime created_at;
+  int updated_by;
+  DateTime updated_at;
+  bool is_active;
+}
