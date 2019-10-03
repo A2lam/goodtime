5614d19b51492fd70697102aa4fdb0d54b0af139
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-10-03 21:11:21 +0200

    Bar : Adding description to the model

--- lib/models/Bar.dart
+++ lib/models/Bar.dart
@@ -5,17 +5,19 @@ class Bar
 {
   int id;
   String name;
+  String description;
   String phone;
   String type;
   User user;
   Address address;
 
-  Bar({ this.id, this.name, this.phone, this.type, this.user, this.address });
+  Bar({ this.id, this.name, this.description, this.phone, this.type, this.user, this.address });
 
   factory Bar.fromJson(Map<String, dynamic> json) {
     return Bar(
       id: json['id'],
       name: json['name'],
+      description: json['description'],
       phone: json['phone'],
       type: json['type'],
       user: User.fromJson(json['user']),
