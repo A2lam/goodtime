9e39acf055cf2929a223c7c3235f5a0b727cc1c6
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-10-06 00:45:50 +0200

    Bar Map Page : Added markers for the differents bar on the map

--- lib/routes/MapBarRoute.dart
+++ lib/routes/MapBarRoute.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:goodtime/models/Bar.dart';
+import 'package:goodtime/services/BarService.dart';
 
 class MapBarRoute extends StatefulWidget
 {
   MapBarRoute({ Key key }) : super(key: key);
+
+  final BarService _barService = new BarService();
 
   @override
   State<StatefulWidget> createState() => new _MapBarRouteState();
@@ -11,11 +15,27 @@ class MapBarRoute extends StatefulWidget
 
 class _MapBarRouteState extends State<MapBarRoute>
 {
-  GoogleMapController mapController;
-  final LatLng _center = const LatLng(48.859201, 2.342820);
+  Map<String, Marker> _markers = {};
+  final LatLng _initialMapPosition = const LatLng(48.859201, 2.342820);
 
+  /// Map Initialization
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    widget._barService.getBars().then((bars) => {
+      setState(() {
+        _markers.clear();
+        for (Bar bar in bars) {
+          final marker = Marker(
+            markerId: MarkerId(bar.id.toString()),
+            position: LatLng(bar.address.latitude, bar.address.longitude),
+            infoWindow: InfoWindow(
+              title: bar.name,
+              snippet: bar.address.number.toString() + " " + bar.address.street + ", " + bar.address.post_code.toString() + " " + bar.address.city,
+            ),
+          );
+          _markers[bar.id.toString()] = marker;
+        }
+      })
+    });
   }
 
   @override
@@ -24,9 +44,10 @@ class _MapBarRouteState extends State<MapBarRoute>
       body: GoogleMap(
         onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(
-          target: _center,
+          target: _initialMapPosition,
           zoom: 11.0,
         ),
+        markers: _markers.values.toSet(),
       ),
     );
   }
