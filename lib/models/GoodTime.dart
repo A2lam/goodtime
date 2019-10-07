cb27cc559e5ea07a4003dd7e32d82dbfdbd66779
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-10-07 23:47:57 +0200

    Switch bar n group into bars_id and groups_id for the moment

--- lib/models/GoodTime.dart
+++ lib/models/GoodTime.dart
@@ -4,21 +4,21 @@ import 'package:goodtime/models/Group.dart';
 class GoodTime
 {
   int id;
-  Group group;
-  Bar bar;
+  int groups_id;
+  int bars_id;
   DateTime date;
   bool is_booking;
   int number_of_participants;
   String status;
   String refusal_purpose;
 
-  GoodTime({ this.id, this.group, this.bar, this.date, this.is_booking, this.number_of_participants, this.status, this.refusal_purpose });
+  GoodTime({ this.id, this.groups_id, this.bars_id, this.date, this.is_booking, this.number_of_participants, this.status, this.refusal_purpose });
 
   factory GoodTime.fromJson(Map<String, dynamic> json) {
     return GoodTime(
         id: json['id'],
-        group: Group.fromJson(json['group']),
-        bar: Bar.fromJson(json['bar']),
+        groups_id: json['groups_id'],
+        bars_id: json['bars_id'],
         date: json['phone'],
         is_booking: json['type'],
         number_of_participants: json['number_of_participants'],
