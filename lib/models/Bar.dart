95df89b35f125f8ba96ce999c22155db29d88f89
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-10-10 00:48:05 +0200

    Check if association arent null before call they factory

--- lib/models/Bar.dart
+++ lib/models/Bar.dart
@@ -14,14 +14,24 @@ class Bar
   Bar({ this.id, this.name, this.description, this.phone, this.type, this.user, this.address });
 
   factory Bar.fromJson(Map<String, dynamic> json) {
+    if (json["user"] != null && json["address"] != null) {
+      return Bar(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        phone: json['phone'],
+        type: json['type'],
+        user: User.fromJson(json['user']),
+        address: Address.fromJson(json['address'])
+      );
+    }
+
     return Bar(
       id: json['id'],
       name: json['name'],
       description: json['description'],
       phone: json['phone'],
       type: json['type'],
-      user: User.fromJson(json['user']),
-      address: Address.fromJson(json['address'])
     );
   }
 }
