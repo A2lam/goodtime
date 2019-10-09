fbfc3c5dd58ca0806c79ec260a3ed717ddfe9c6e
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-10-10 00:48:38 +0200

    Check if association arent null before calling they factory

--- lib/models/GoodTime.dart
+++ lib/models/GoodTime.dart
@@ -12,18 +12,52 @@ class GoodTime
   String status;
   String refusal_purpose;
 
-  GoodTime({ this.id, this.groups_id, this.bars_id, this.date, this.is_booking, this.number_of_participants, this.status, this.refusal_purpose });
+  Group group;
+  Bar bar;
+
+  GoodTime({ this.id, this.groups_id, this.bars_id, this.date, this.is_booking, this.number_of_participants, this.status, this.refusal_purpose, this.group, this.bar });
 
   factory GoodTime.fromJson(Map<String, dynamic> json) {
+    if (json["bar"] != null && json["group"] != null) {
+      return GoodTime(
+          id: json['id'],
+          groups_id: json['groups_id'],
+          bars_id: json['bars_id'],
+          date: DateTime.parse(json['date']),
+          is_booking: json['is_booking'] == 1 ? true : false,
+          number_of_participants: json['number_of_participants'],
+          status: json['status'],
+          refusal_purpose: json['refusal_purpose'],
+          group: Group.fromJson(json['group']),
+          bar: Bar.fromJson(json['bar'])
+      );
+    }
+
+    else if (json["bar"] != null && json["group"] == null) {
+      return GoodTime(
+          id: json['id'],
+          groups_id: json['groups_id'],
+          bars_id: json['bars_id'],
+          date: DateTime.parse(json['date']),
+          is_booking: json['is_booking'] == 1 ? true : false,
+          number_of_participants: json['number_of_participants'],
+          status: json['status'],
+          refusal_purpose: json['refusal_purpose'],
+          bar: Bar.fromJson(json['bar'])
+      );
+    }
+
     return GoodTime(
         id: json['id'],
         groups_id: json['groups_id'],
         bars_id: json['bars_id'],
-        date: json['phone'],
-        is_booking: json['type'],
+        date: DateTime.parse(json['date']),
+        is_booking: json['is_booking'] == 1 ? true : false,
         number_of_participants: json['number_of_participants'],
         status: json['status'],
         refusal_purpose: json['refusal_purpose'],
+        group: null,
+        bar: null
     );
   }
 }
