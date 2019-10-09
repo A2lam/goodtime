185cd4f7bdbca411f76d705ab1c40f04e6219b53
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-10-10 00:49:40 +0200

    Implements get reservations route

--- lib/services/ReservationService.dart
+++ lib/services/ReservationService.dart
@@ -9,6 +9,7 @@ class ReservationService
   final String _baseUrl = "http://10.0.2.2:3000/good_times";
   final _storage = new FlutterSecureStorage();
 
+  /// Create a reservation
   Future<GoodTime> createReservation(int bars_id, DateTime date, int number_of_participants, { int groups_id = null }) async {
     String token = await _storage.read(key: "token");
     GoodTime goodTime;
@@ -40,5 +41,28 @@ class ReservationService
     }
 
     return goodTime;
+  }
+
+  /// Get all user's reservation
+  Future<List<GoodTime>> getReservations() async {
+    String token = await _storage.read(key: "token");
+    List<GoodTime> reservationList = new List<GoodTime>();
+    var reservations;
+
+    var response = await http.get(_baseUrl, headers: { HttpHeaders.authorizationHeader: "bearer " + token });
+
+    if (response.statusCode == 200) {
+      reservations = convert.jsonDecode(response.body);
+      for (var reservation in reservations) {
+        reservationList.add(GoodTime.fromJson(reservation));
+      }
+    }
+    else {
+      print("Request failed : ${response.statusCode}");
+
+      return null;
+    }
+
+    return reservationList;
   }
 }
